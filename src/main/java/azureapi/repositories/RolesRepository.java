fbdd7de9c8bff3bb9ef7b2a8429a2f3b0e96ba7e
hawk <alancamey@gmail.com>
2022-06-19 13:46:30 -0400

    cambios para que lea la db remota

---
+++ src/main/java/azureapi/repositories/RolesRepository.java
@@ -0,0 +1,21 @@
+package azureapi.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import azureapi.Models.Roles;
+
+public interface RolesRepository extends JpaRepository<Roles, Long> {
+	public Optional<Roles> findByNombre(String nombre);
+	public Optional<Roles> findById(int id); //add today 03/31/22
+	public Boolean existsById(int id); //add today 03/31/22
+	//public Boolean notExistsByRol_id(int rol_id);//add today 03/31/22
+//	return new ResponseEntity<>("Usuario registrado con exito",HttpStatus.OK);
+	
+	public void deleteById(Integer id);
+	//public Integer save();
+	
+	//public Optional <Rol> getAllRols();
+	
+}
